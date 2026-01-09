377ffa893e961e46e4dfbb3b912c843fe42d838e
qwerty-asci <pablogacj.trabajo@gmail.com>
2026-01-09 11:41:22 +0100

    Cambios

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -3,7 +3,7 @@
 filesdir=$1
 searchstr=$2
 
-if [ $# -ne 2 ]
+if [$# -ne 2]
 then
 
     echo "The parameters are not specified"
@@ -11,8 +11,18 @@ then
 fi
 
 
-if [ -e $filesdir];
+if [-d $filesdir]
 then
-
+    echo "The directoy doesn't exist"
+    exit 1
 fi
+
+trap echo "" EXIT
+
+archivos=$(find $filesdir)
+
+coincidencias=$(cat $archivos |grep -c $searchstr)
+
+
+echo "The number of files are ${#archivos[@]} and the number of matching lines are ${coincidencias}"
 
