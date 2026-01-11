260d799ff94ed77a45a637f8cb4dbe66272315f2
qwerty-asci <pablogacj.trabajo@gmail.com>
2026-01-11 11:53:20 +0100

    Implementacion de writer.c

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -1,10 +1,44 @@
 #include <stdio.h>
-#include <cstdlib.h>
+#include <stdlib.h>
+#include <errno.h>
 
 
 int main(int argc,char * argv[]){
 
 
+	char * ruta_del_archivo,* texto;
+	FILE * pFile;
 
-	return 0;
+	if(argc!=3){
+		return EXIT_FAILURE;
+	}
+
+	ruta_del_archivo=argv[1];
+	texto=argv[2];
+
+
+	pFile=fopen(ruta_del_archivo,"w");
+
+	if(errno==0){
+
+		fputs (texto,pFile);
+
+		fclose(pFile);
+	}else if(errno==2 && errno==6){
+		printf("The file doesn't exist");
+		return EXIT_FAILURE;
+	}else if(errno==13){
+		printf("Permisson denied");
+		return EXIT_FAILURE;
+	}else{
+		printf("Something unexpected happens working with the file");
+		return EXIT_FAILURE;
+	}
+
+
+
+
+
+
+	return EXIT_SUCCESS;
 }
